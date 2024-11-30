4b0d7b943ea3e4a5fd2514d2a2c296d6cc3d74fb
unexploredtest <unexploredtest@tutanota.com>
2024-11-30 19:47:25 +0330

    Check command arqument size

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,22 @@ mod mnist_reader;
 mod network;
 mod train;
 
+const HELP_MESSAGE: &str = " \
+Arquments required: data path, epoch, mini batch size, learning rate  \
+";
+
 
 fn main() {
+    if env::args().count() < 5 {
+        println!("{}", HELP_MESSAGE);
+        return;
+    }
+
     let args: Vec<String> = env::args().collect();
     let data_path = &args[1];
     let epoch = args[2].parse::<u32>().unwrap();
     let mini_batch_size = args[3].parse::<u32>().unwrap();
     let eta = args[4].parse::<f32>().unwrap();
-    
+
     train::train(data_path, epoch, mini_batch_size, eta);
 }
